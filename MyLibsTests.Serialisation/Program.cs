c481d777e723ce69a3b31be376aa9273c3676da6
dilannanfack <dilannanfack@gmail.com>
2021-11-24 00:27:30 +0100

    Ajoutez des fichiers projet.

---
+++ MyLibsTests.Serialisation/Program.cs
@@ -0,0 +1,17 @@
+﻿using MyLibs.Serialisation;
+using System;
+
+namespace MyLibsTests.Serialisation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serializer sz = new Serializer();
+            var data = new Person{ Name = "Toto", Age = 65 };
+            sz.Serialize<Person>(data, "Person.bin");
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+    }
+}
